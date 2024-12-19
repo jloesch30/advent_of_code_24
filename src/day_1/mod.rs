fdfa6c686d45182a5911222b00ea88dfd37c8def
Josh Loesch <jloesch@apexclearing.com>
2024-12-19 14:17:48 -0600

    1: part 2

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -1,12 +1,17 @@
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader};
 use std::iter::zip;
 
-pub fn part_1() -> Result<String, io::Error> {
+fn read_file() -> io::Result<BufReader<File>> {
     let path = "src/day_1/input/puzzle_input.txt";
     let file = File::open(path)?;
 
-    let reader = io::BufReader::new(file);
+    Ok(io::BufReader::new(file))
+}
+
+pub fn part_1() -> Result<String, io::Error> {
+    let reader = read_file()?;
 
     let mut column1 = Vec::new();
     let mut column2 = Vec::new();
@@ -29,5 +34,41 @@ pub fn part_1() -> Result<String, io::Error> {
         sum += (a - b).abs();
     }
 
+    Ok(sum.to_string())
+}
+
+// We need to find the similarity score of the left list with the right list
+// this can be achieved by creating a hash map of the right list with the number and how many times
+// it appears, and then iterate through the left list and multiply the value with how many
+// occurrences from the hash map
+pub fn part_2() -> Result<String, io::Error> {
+    let reader = read_file()?;
+
+    let mut column1 = Vec::new();
+    let mut column2 = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        column1.push(parts[0].parse::<i32>().unwrap());
+        column2.push(parts[1].parse::<i32>().unwrap());
+    }
+
+    // Create a hash map of the right list
+    let mut hash_map: HashMap<i32, i32> = HashMap::new();
+
+    for num in column2 {
+        *hash_map.entry(num).or_insert(0) += 1;
+    }
+
+    // For each value in the left column, we need to multiply the value with the number of
+    // occurrences
+    
+    let mut sum = 0;
+    for num in column1 {
+        if let Some(value) = hash_map.get(&num) {
+            sum += num * value;
+        } 
+    }
     Ok(sum.to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ pub mod day_1;
 
 fn main() {
     println!("day_1::part_1: {}", day_1::part_1().unwrap());
+    println!("day_1::part_2: {}", day_1::part_2().unwrap());
 }
